4c39c533e6ca4df5cd32dfa4d515615baa1a383f
Brian Nicholls <nichol88@gmail.com>
2019-12-13 04:35:57 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,30 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    a = []
+    # check for match in each item
+    array.each{ |w|
+      if char_count(w) == char_count(@word)
+        a.push(w)
+      end
+    }
+    a
+  end
+
+  private
+
+  def char_count(word) # returns sorted hash of character count
+    h = {}
+    word.each_char { |chr|
+      h[chr] = word.count(chr)
+    }
+    h.sort
+  end
+
+end
